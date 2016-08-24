263ec2ac002c5275f73589fd8c2c525e3ec701ae
Jesse Clark <jesse.clark@unco.edu>
2016-08-24 09:08:48 -0600

    Add class and testing class.

---
+++ src/MyClassTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyClassTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		
+		assertEquals("10 x 10 = 100", 100, 10*10);
+		
+	}
+
+}
