b4b4f8f62d72f1222e643e85e738b6c34bf2228f
Jesse Clark <jesse.clark@unco.edu>
2016-08-24 09:12:55 -0600

    Add function and testing class.

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,14 @@
 
 public class MyClass {
 
+	//Constructor:
+	public MyClass(){
+		System.out.print("MyClass Constructor!");
+	}
+	
+	public int multiply(int x1, int x2){
+		
+		return x1*x2;
+	}
+	
 }
--- src/MyClassTest.java
+++ src/MyClassTest.java
@@ -3,12 +3,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyClassTest {
-
+	
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
 		
+		MyClass mc = new MyClass();
+		
 		assertEquals("10 x 10 = 100", 100, 10*10);
+		assertEquals("10 x 0 = 0", 0, mc.multiply(100, 0));
+		
 		
 	}
 
