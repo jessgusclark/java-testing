904296ef48a8aae176d9dbab326ff453f79cf49c
Jesse Clark <contact@jessclark.com>
2016-08-27 12:14:37 -0600

    Modify exception in catch().

--- src/exception/Cafe.java
+++ src/exception/Cafe.java
@@ -7,11 +7,17 @@ public class Cafe {
 		try{
 			customer.drinkCoffee(coffee);
 			System.out.println("Customer Drank Coffee");
-		}catch(TooHotException e){
+		}catch(TemperatureException e){
+			System.out.println("Coffee was the wrong temperature!");
+		}
+		/*
+		 * Unreachable:
+		catch(TooHotException e){
 			System.out.println("Coffee was too hot!" );
 		}catch(TooColdException e){
 			System.out.println("Coffee was too cold!" );
 		}
+		*/
 		
 	}
 }
