e7adf10272a912de018f32b2bf1180e4aa954f29
Jesse Clark <contact@jessclark.com>
2016-08-27 11:12:53 -0600

    Add Exceptions for temperature, too hot and too cold.

--- src/exception/Main.java
+++ src/exception/Main.java
@@ -3,5 +3,11 @@ package exception;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Custom Exception!");
+		
+		try{
+			throw new TooHotException();
+		}catch(TemperatureException e){
+			System.out.println("Error: " + e);
+		}
 	}
 }
